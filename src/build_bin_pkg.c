f17f8f7ef2358e18164edb7d2ab8f62b2815c550
Omar Berrow <omarkberrow@gmail.com>
2025-10-25 14:22:00 -0400

    Use XBPS instead of tar to build binary packages

--- src/build_bin_pkg.c
+++ src/build_bin_pkg.c
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #include <assert.h>
 #include <sys/stat.h>
+#include <string.h>
 
 #include "package.h"
 #include "lock.h"
@@ -43,66 +44,155 @@ static void create_pkg(package* pkg, bool build_dependencies)
 
     (void)package_make_bin_prefix(pkg);
 
+    struct stat st = {};
+    if (stat(pkg->bin_package_prefix, &st) == -1)
+    {
+        unlock();
+        perror("stat");
+        return;
+    }
+    if (!S_ISDIR(st.st_mode))
+    {
+        unlock();
+        fprintf(stderr, "%s is not a directory\n", pkg->bin_package_prefix);
+        return;
+    }
 #ifndef NDEBUG
-    printf("Entering directory %s\n", pkg->bin_package_prefix);
+    printf("Entering directory %s\n", binary_package_directory);
 #endif
-    if (chdir(pkg->bin_package_prefix) == -1)
+    if (chdir(binary_package_directory) == -1)
     {
         unlock();
         perror("chdir");
         return;
     }
 
-    char *bin_package_path = NULL;
-    size_t bin_package_path_len = 0;
-    bin_package_path_len = snprintf(NULL,0, "%s/%s-%d.%d.%d.tar", 
-        binary_package_directory,
-        pkg->name,
-        pkg->version.arr[0], pkg->version.arr[1], pkg->version.arr[2]);
-    bin_package_path = malloc(bin_package_path_len+1);
-    snprintf(bin_package_path, bin_package_path_len+1, "%s/%s-%d.%d.%d.tar", 
-        binary_package_directory,
-        pkg->name,
-        pkg->version.arr[0], pkg->version.arr[1], pkg->version.arr[2]);
-
-    struct stat st = {};
-    if (stat(bin_package_path, &st) == 0)
+    command xbps_create_cmd = {.proc="xbps-create"};
+    command xbps_rindex_cmd = {.proc="xbps-rindex"};
+    
+    const char* triplet = pkg->host_package ? g_config.host_triplet : g_config.target_triplet;
+    size_t len_arch = strchr(triplet, '-') - triplet;
+    char* architecture = memcpy(malloc(len_arch+1), triplet, len_arch);
+    architecture[len_arch] = 0;
+    size_t package_version_len = snprintf(NULL, 0, "%s-%d.%d_%d", pkg->name, pkg->version.major, pkg->version.minor, pkg->version.patch);
+    char* package_version = malloc(package_version_len+1);
+    snprintf(package_version, package_version_len+1, "%s-%d.%d_%d", pkg->name, pkg->version.major, pkg->version.minor, pkg->version.patch);
+    size_t depends_str_len = 0;
+    for (size_t i = 0; i < pkg->depends.cnt; i++)
     {
-        struct pkginfo* info = read_package_info(pkg->name);
-        struct timeval install_time = info->install_date;
-        struct timeval mtim = {};
-        free(info);
-        mtim.tv_sec = st.st_mtim.tv_sec;
-        mtim.tv_usec = st.st_mtim.tv_nsec/1000;
-        if (timercmp(&mtim, &install_time, >=))
+        union package_version version;
+        char* depend_expr = pkg->depends.buf[i];
+        char* depend = NULL;
+        int how_cmp = 0;
+        parse_depend_expr(depend_expr, &depend, &version, &how_cmp);
+        if (!depend)
         {
-            chdir(root_directory);
+            fprintf(stderr, "%s: Invalid dependency expression %s in package %s while building binary package. Aborting\n", g_argv[0], depend_expr, pkg->name);
+            unlock();
             return;
         }
+        package* depend_pkg = get_package(depend);
+        if (!depend_pkg)
+        {
+            fprintf(stderr, "%s: Missing/invalid dependency %s in package %s while building binary package. Aborting\n", g_argv[0], depend_expr, pkg->name);
+            unlock();
+            return;
+        }
+        if (!do_version_cmp(how_cmp, depend_pkg->version, version))
+        {
+            fprintf(stderr, "%s: Could not satisfy dependency. Requires: %s, got: %s=%d.%d.%d. Aborting\n",
+                g_argv[0],
+                depend_expr,
+                depend_pkg->name,
+                depend_pkg->version.major, depend_pkg->version.minor, depend_pkg->version.patch
+            );
+            unlock();
+            return;
+        }
+        if (depend_pkg->supports_binary_packages)
+            depends_str_len += strlen(depend_expr)+1;
+        free(depend_pkg);
+    }
+    if (depends_str_len)
+        depends_str_len++;
+    char* depends_str = depends_str_len ? malloc(depends_str_len) : NULL;
+    if (depends_str)
+    {
+        depends_str[0] = 0;
+        for (size_t i = 0; i < pkg->depends.cnt; i++)
+        {
+            union package_version version;
+            char* depend_expr = pkg->depends.buf[i];
+            char* depend = NULL;
+            int how_cmp = 0;
+            parse_depend_expr(depend_expr, &depend, &version, &how_cmp);
+            if (!depend)
+            {
+                fprintf(stderr, "%s: Invalid dependency expression %s in package %s while building binary package. Aborting\n", g_argv[0], depend_expr, pkg->name);
+                unlock();
+                return;
+            }
+            package* depend_pkg = get_package(depend);
+            if (depend_pkg->supports_binary_packages)
+            {
+                strcat(depends_str, depend_expr);
+                strcat(depends_str, " ");
+            }
+            free(depend_pkg);
+        }
+        depends_str[depends_str_len-1] = 0;
     }
 
-    command_array run_tar = {};
-    command tar_cmd = {.proc="tar"};
-    command gz_cmd = {.proc="gzip"};
+    string_array_append(&xbps_create_cmd.argv, xbps_create_cmd.proc);
+    if (depends_str)
+    {
+        string_array_append(&xbps_create_cmd.argv, "-D");
+        string_array_append(&xbps_create_cmd.argv, depends_str);
+    }
+    string_array_append(&xbps_create_cmd.argv, "-q");
+    string_array_append(&xbps_create_cmd.argv, "-A");
+    string_array_append(&xbps_create_cmd.argv, architecture);
+    string_array_append(&xbps_create_cmd.argv, "-n");
+    string_array_append(&xbps_create_cmd.argv, package_version);
+    string_array_append(&xbps_create_cmd.argv, "-s");
+    string_array_append(&xbps_create_cmd.argv, pkg->description);
+    string_array_append(&xbps_create_cmd.argv, pkg->bin_package_prefix);
+
+    size_t len_package_filename = snprintf(NULL, 0, "%s.%s.xbps", package_version, architecture);
+    char* package_filename = malloc(len_package_filename+1);
+    snprintf(package_filename, len_package_filename+1, "%s.%s.xbps", package_version, architecture);
+
+    free(architecture);
+    free(package_version);
+    free(depends_str);
+
+    int cmd_ret = run_command(xbps_create_cmd.proc, xbps_create_cmd.argv);
+
+    if (cmd_ret != 0)
+    {
+        fprintf(stderr, "%s returned %d\n", xbps_create_cmd.proc, cmd_ret);
+        goto done;
+    }
+
+    string_array_append(&xbps_rindex_cmd.argv, xbps_rindex_cmd.proc);
+    string_array_append(&xbps_rindex_cmd.argv, "-f");
+    string_array_append(&xbps_rindex_cmd.argv, "-a");
+    string_array_append(&xbps_rindex_cmd.argv, package_filename);
+
+    cmd_ret = run_command(xbps_rindex_cmd.proc, xbps_rindex_cmd.argv);
     
-    string_array_append(&tar_cmd.argv, "tar");
-    string_array_append(&tar_cmd.argv, "-cf");
-    string_array_append(&tar_cmd.argv, bin_package_path);
-    string_array_append(&tar_cmd.argv, "* >/dev/null 2>&1");
+    if (cmd_ret != 0)
+    {
+        fprintf(stderr, "%s returned %d\n", xbps_create_cmd.proc, cmd_ret);
+        goto done;
+    }
 
-    string_array_append(&gz_cmd.argv, "gzip");
-    string_array_append(&gz_cmd.argv, "-qf9");
-    string_array_append(&gz_cmd.argv, bin_package_path);
+    free(package_filename);
 
-    command_array_append(&run_tar, &tar_cmd);
-    command_array_append(&run_tar, &gz_cmd);
-
-    command_array_run(&run_tar, NULL);
-
-    command_array_free(&run_tar);
-
-    free(bin_package_path);
-
+    done:
+#ifndef NDEBUG
+    printf("Entering directory %s\n", root_directory);
+#endif
     chdir(root_directory);
 }
 
